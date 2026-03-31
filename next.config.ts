cb05ef1e0a41ff54c19cfb1a612acabaa4bee8b2
Quan Tran <quan.tran@emandai.net>
2026-03-31 15:25:21 +0700

    make simple for next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
-  compress: true,
-  images: {
-    unoptimized: true,
-  },
-  turbopack: {
-    root: process.cwd(),
-  },
 };
 
 export default nextConfig;
